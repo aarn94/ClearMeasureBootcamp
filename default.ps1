5a573f1a434f5ec7d297b16d8bd31b3931ce4a94
aarn94 <adrian.adamiak.priv@gmail.com>
2018-04-15 20:09:52 +0200

    change

--- default.ps1
+++ default.ps1
@@ -30,7 +30,7 @@ properties {
     $schemaDatabaseName = $databaseName + "_schema"
     $integratedSecurity = "Integrated Security=fa"
     
-    $connection_string = "server=$databaseserver;database=$databasename;integrated security=false;User Id=$databaseLogin;Password=$databasePassword"
+    $connection_string = "server=$databaseserver;database=$databasename;integrated security=true"
     $AliaSql = "$source_dir\Database\scripts\AliaSql.exe"
     $webapp_dir = "$source_dir\UI"
 
